5084686a6aaaddd04ba6409de01723242f62fb01
Yukiji Nakatsuka <lunarbase@outlook.com>
2019-02-10 18:48:41 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -20,5 +20,5 @@ def select_series_title_with_most_human_characters
 end
 
 def select_character_names_and_number_of_books_they_are_in
-  "SELECT character.name, COUNT(character_books.book_id) FROM characters INNER JOIN character_books GROUP BY characters.name ORDER BY COUNT(character_books.book_id) DESC;"
+  "SELECT characters.name, COUNT(character_books.book_id) FROM characters INNER JOIN character_books GROUP BY characters.name ORDER BY COUNT(character_books.book_id) DESC;"
 end
